d8120522a05a039460ce261ff7f34c8bbc90a43f
Vasil Angelov <vasilangelov83@gmail.com>
2022-03-24 23:08:42 +0100

    50/100 so far :(

--- SoftUni Exams Preparation/Password Reset/password_reset.py
+++ SoftUni Exams Preparation/Password Reset/password_reset.py
@@ -1,40 +1,32 @@
 
 
 string = input()
-password = ""
+new_pass = string
 
 while True:
     command = input()
     if command == "Done":
         break
     if command == "TakeOdd":
-        for i in range(len(string)):
-            if i % 2 != 0:
-                password += string[i]
-        print(password)
+        current_pass = new_pass
+        new_pass = ""
+        for i in range(1, len(current_pass), 2):
+            new_pass += current_pass[i]
+        print(new_pass)
 
     command = command.split(" ")
     if command[0] == "Cut":
         index = int(command[1])
         length = int(command[2])
-        if index >= 0:
-            to_remove = password[index:(index+length)]
-
-        else:
-            if index < -1:
-                to_remove = password[(index - length):index]
-
-            else:
-                to_remove = password[(index - length):index]
-        password = password.replace(to_remove, "", 1)
-        print(password)
+        new_pass = new_pass[:index] + new_pass[(index+length):]
+        print(new_pass)
 
     if command[0] == "Substitute":
         substring = command[1]
         substitute = command[2]
-        if substring in password:
-            password = password.replace(substring, substitute)
-            print(password)
+        if substring in new_pass:
+            new_pass = new_pass.replace(substring, substitute)
+            print(new_pass)
         else:
             print("Nothing to replace!")
-print(f"Your password is: {password}")
+print(f"Your password is: {new_pass}")
