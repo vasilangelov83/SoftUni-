e6a853e5c902cd18226743a9b7a3906b6761c8a0
Vasil Angelov <vasilangelov83@gmail.com>
2022-04-12 23:17:39 +0200

    hello world tkinter

--- sandbox/hello_world_tkinter.py
+++ sandbox/hello_world_tkinter.py
@@ -1,6 +1,15 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.title("TK Window Centre")
+window_height = 300
+window_width = 300
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+center_x = int(screen_width/2 - window_width/2)
+center_y = int(screen_height/2 - window_height/2)
+root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
 message = tk.Label(root, text = "Hello World!")
 message.pack()
 
