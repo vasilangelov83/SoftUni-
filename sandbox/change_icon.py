e8ed9b7448b56a719b24b60f07c5c580b218417c
Vasil Angelov <vasilangelov83@gmail.com>
2022-04-12 23:41:17 +0200

    change icon

---
+++ sandbox/change_icon.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root =tk.Tk()
+root.title("Test window")
+root.geometry("600x400+50+50")
+root.resizable(False, False)
+root.iconbitmap('./apple_fruit_food_icon_218349.ico')
+
+root.mainloop()
