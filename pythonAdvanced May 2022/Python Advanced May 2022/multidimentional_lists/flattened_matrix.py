02e28a7fd6dcc29bde50736041d4ed93792de38c
Vasil Angelov <vasilangelov83@gmail.com>
2022-06-04 13:57:30 +0300

    matix

--- pythonAdvanced May 2022/Python Advanced May 2022/multidimentional_lists/flattened_matrix.py
+++ pythonAdvanced May 2022/Python Advanced May 2022/multidimentional_lists/flattened_matrix.py
@@ -6,5 +6,5 @@ n = int(input())
 
 matrix = [input().split(', ') for _ in range(n)]
 
-flat_matrix = [num for row in matrix for num in row]
+flat_matrix = [int(num) for row in matrix for num in row]
 print(flat_matrix)
